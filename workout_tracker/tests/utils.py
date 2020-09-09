721e8a0cc8224253f10933790d1b66270b89866b
Eric Dang <edang830@gmail.com>
2020-09-09 22:43:30 +0000

    Add default pagination to settings

--- workout_tracker/tests/utils.py
+++ workout_tracker/tests/utils.py
@@ -1,5 +1,7 @@
 import json
 
+from django.conf import settings
+
 from workout_tracker.urls import PREFIX_API_URL
 
 
@@ -14,3 +16,10 @@ def invalidate_credentials(api_client):
 def get_json_file_len(filepath):
     with open(filepath, 'r') as file:
         return len(list(json.load(file)))
+
+
+def get_resp_data(resp):
+    if 'DEFAULT_PAGINATION_CLASS' in settings.REST_FRAMEWORK:
+        return resp.data['results']
+    else:
+        return resp.data
