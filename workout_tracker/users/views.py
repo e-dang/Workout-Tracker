383c5dd70596f6821d8983e13efd07f12213f966
Eric Dang <edang830@gmail.com>
2020-09-09 00:15:10 +0000

    Implement UserViewSet and register the urls

--- workout_tracker/users/views.py
+++ workout_tracker/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 
-# Create your views here.
+from core.views import ListRetrieveUpdateDeleteViewSet
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(ListRetrieveUpdateDeleteViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['username', 'first_name', 'last_name']
+    search_fields = ['username', 'first_name', 'last_name']
+    ordering_fields = ['username', 'first_name', 'last_name']
+    ordering = ['username']
+
+    def perform_destory(self, instance):
+        instance.is_active = False
+        instance.save()
+        return Response(status=HTTP_204_NO_CONTENT)
