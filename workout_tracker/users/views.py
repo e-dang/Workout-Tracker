c904b065c3f7fa35899a315ce1c708f7a0133384
Eric Dang <edang830@gmail.com>
2020-09-09 05:24:36 +0000

    Fix mis-spelling in perform_destroy()

--- workout_tracker/users/views.py
+++ workout_tracker/users/views.py
@@ -1,7 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.response import Response
-from rest_framework.status import HTTP_204_NO_CONTENT
 
 from core.views import ListRetrieveUpdateDeleteViewSet
 
@@ -18,7 +16,6 @@ class UserViewSet(ListRetrieveUpdateDeleteViewSet):
     ordering_fields = ['username', 'first_name', 'last_name']
     ordering = ['username']
 
-    def perform_destory(self, instance):
+    def perform_destroy(self, instance):
         instance.is_active = False
         instance.save()
-        return Response(status=HTTP_204_NO_CONTENT)
