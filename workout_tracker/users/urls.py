84995f2e016fb70ab4121128882d66c958f8134d
Eric Dang <edang830@gmail.com>
2020-09-11 01:46:18 +0000

    Add user-movements-list url

--- workout_tracker/users/urls.py
+++ workout_tracker/users/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 from equipment.views import UserEquipmentListCreateView
+from movements.views import UserMovementListCreateView
 
 router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
-    path('<int:pk>/equipment/', UserEquipmentListCreateView.as_view(), name='user-equipment-list')
+    path('<int:pk>/equipment/', UserEquipmentListCreateView.as_view(), name='user-equipment-list'),
+    path('<int:pk>/movements/', UserMovementListCreateView.as_view(), name='user-movement-list')
 ]
 urlpatterns += router.urls
