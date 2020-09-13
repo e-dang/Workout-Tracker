b85979b88a53aee964038e7dd7e90f1c988a7de8
Eric Dang <edang830@gmail.com>
2020-09-13 21:57:00 +0000

    Add exercise to installed apps and admin site

--- workout_tracker/exercises/admin.py
+++ workout_tracker/exercises/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import ExerciseTemplate, Exercise, WorkloadTemplate, Workload, SetTemplate, Set
 
-# Register your models here.
+
+class ExerciseTemplateAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkloadTemplateAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkloadAdmin(admin.ModelAdmin):
+    pass
+
+
+class SetTemplateAdmin(admin.ModelAdmin):
+    pass
+
+
+class SetAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ExerciseTemplate, ExerciseTemplateAdmin)
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(WorkloadTemplate, WorkloadTemplateAdmin)
+admin.site.register(Workload, WorkloadAdmin)
+admin.site.register(SetTemplate, SetTemplateAdmin)
+admin.site.register(Set, SetAdmin)
