0170c8866ea98af74fb5b277d9f1ee1600110a42
Eric Dang <edang830@gmail.com>
2020-09-28 02:36:33 +0000

    Change serializers to HyperlinkedModelSerializers

--- workout_tracker/muscles/serializers.py
+++ workout_tracker/muscles/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import MuscleSubportion, Muscle, MuscleGrouping
 
 
-class MuscleSubportionSerializer(serializers.ModelSerializer):
+class MuscleSubportionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MuscleSubportion
         fields = ['name']
 
 
-class MuscleSerializer(serializers.ModelSerializer):
+class MuscleSerializer(serializers.HyperlinkedModelSerializer):
     sub = MuscleSubportionSerializer(source='subportions', many=True)
 
     class Meta:
@@ -16,9 +16,12 @@ class MuscleSerializer(serializers.ModelSerializer):
         fields = ['name', 'sub']
 
 
-class MuscleGroupingSerializer(serializers.ModelSerializer):
+class MuscleGroupingSerializer(serializers.HyperlinkedModelSerializer):
     sub = MuscleSerializer(source='muscles', many=True)
 
     class Meta:
         model = MuscleGrouping
-        fields = ['name', 'snames', 'sub']
+        fields = ['url', 'name', 'snames', 'sub']
+        extra_kwargs = {
+            'url': {'view_name': 'muscle-detail', 'lookup_field': 'pk'}
+        }
