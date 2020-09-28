56b5b7639c3071c054d654b964c882130149d077
Eric Dang <edang830@gmail.com>
2020-09-28 02:37:31 +0000

    Make snames a read only field

--- workout_tracker/movements/serializers.py
+++ workout_tracker/movements/serializers.py
@@ -6,6 +6,7 @@ class MovementSerializer(OwnedMultiAliasResourceSerializer):
     class Meta:
         model = Movement
         fields = extend_fields(OwnedMultiAliasResourceSerializer, ['muscles', 'equipment'])
+        read_only_fields = ['snames']
         extra_kwargs = {
             'muscles': {'view_name': 'muscle-detail', 'lookup_field': 'pk'}
         }
